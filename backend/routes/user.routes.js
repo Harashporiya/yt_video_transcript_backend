9a67c1ad1d93df86354da930577f2e5510885a27
Harash Poriya <harash.poriya@gmail.com>
2026-05-23 11:17:16 +0530

    route google

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,4 +1,4 @@
-import { signUp, login, getUserVideos, getUserProfile } from "../controllers/user.controllers.js";
+import { signUp, login, googleAuth, getUserVideos, getUserProfile } from "../controllers/user.controllers.js";
 import { Router } from "express";
 import { authenticateToken } from "../middlewares.js";
 
@@ -6,6 +6,7 @@ const router=Router();
 
 router.post("/signup",signUp);
 router.post("/login",login);
+router.post("/google",googleAuth);
 router.get("/videos", authenticateToken, getUserVideos);
 router.get("/profile", authenticateToken, getUserProfile);
 
