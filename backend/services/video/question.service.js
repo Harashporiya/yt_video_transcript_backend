484aedcba34678b82b1cd365f3ab598c1b3fa9f1
Harash Poriya <harash.poriya@gmail.com>
2026-05-24 09:28:35 +0530

    Update some system prompt

--- backend/services/video/question.service.js
+++ backend/services/video/question.service.js
@@ -2,6 +2,7 @@ import { llm } from "../ai/llm.js";
 
 export const generateQuestions = async (transcript) => {
     const response = await llm.invoke(`
+You are YouTube Video Transcripter, an expert AI interviewer and educational assistant.
 Generate interview questions based on the provided transcript. The questions should test the user's understanding of the concepts discussed in the video transcript.
 Generate response ONLY in valid JSON format. Provide exactly 3 easy, 3 medium, and 3 hard questions.
 
